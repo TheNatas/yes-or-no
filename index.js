7f875bcc7b67dc9619823985f334caf9683146b3
Natanael <natasnael2002@gmail.com>
2023-02-01 22:08:22 -0300

    handle no answer

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 const handleYesClick = () => window.open(`https://wa.me/5548996750255?text=${encodeURI('oiiii eu aceito sair ctggg <3<3')}`);
+
+const handleNoClick = () => window.open('https://google.com/search?q=como ser uma pessoa melhor');
