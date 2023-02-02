681cc83002b5402c0100f17c4af7150c69753b53
Natanael <natasnael2002@gmail.com>
2023-02-01 22:18:35 -0300

    add secret exclusive message on console

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+console.log(
+  'Nesses tempos modernos a gente copia e cola muitas coisas e por isso eu gostaria de dedicar um tempo exclusivamente para você, tal qual você merece, por ter feito meus últimos dias muito melhores. E eu não poderia agradecer de outro jeito se não com as minhas mais sinceras palavras escritas especialmente para você. Obrigado por ser quem você é, Maria Aparecida dos Santos Almeira de Jesus. Espero que nesse ano de 1997 nós possamos estar juntos muitas outras vezes. <3'
+)
+
 const handleYesClick = () => window.open(`https://wa.me/5548996750255?text=${encodeURI('oiiii eu aceito sair ctggg <3<3')}`);
 
 const handleNoClick = () => window.open('https://google.com/search?q=como ser uma pessoa melhor');
